3bf0eaf7bc2fd2bd29ae12c3d09dfe3ef34aec76
Dmitry Sysolyatin <swonl.mail@gmail.com>
2023-03-06 22:19:44 +0200

    added provider type

--- src/weather/provider.rs
+++ src/weather/provider.rs
@@ -1,7 +1,28 @@
+use serde::{Serialize, Deserialize};
+
 use crate::weather::info::WeaterInfo;
+use super::config::Config;
 
 trait Provider {
-    fn get_info<I>(address: &str) -> I
+    fn create<P>(config: Config) -> Self;
+    fn get_type(&self) -> ProviderType;
+    fn get_info<I>(&self, address: &str) -> I
     where 
     I: WeaterInfo;
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ProviderType {
+    OpenWeather
+}
+
+impl ProviderType {
+    pub fn parse(str: &str) -> Result<ProviderType, ()> {
+        match str {
+            "OpenWeather" => {
+                Ok(ProviderType::OpenWeather)
+            },
+            _ => Err(()),
+        }
+    }
 }
