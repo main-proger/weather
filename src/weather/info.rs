cb7dbcef1c22971e00a5c8370da573fc29c9e02e
Dmitry Sysolyatin <swonl.mail@gmail.com>
2023-03-06 17:25:27 +0200

    added convert info to string

--- src/weather/info.rs
+++ src/weather/info.rs
@@ -22,3 +22,49 @@ pub struct Address {
     pub country: Option<String>,
     pub address: String,
 }
+
+pub struct Humidity(f64);
+
+
+
+impl ToString for TempType {
+    fn to_string(&self) -> String {
+        match self {
+            TempType::Farengate => {
+                String::from("°F")
+            },
+            TempType::Kelvin => {
+                String::from("°K")
+            },
+            TempType::Celsius => {
+                String::from("°C")
+            },
+        }
+    }
+}
+
+impl ToString for Temp {
+    fn to_string(&self) -> String {
+        if self.temp_max == self.temp_min {
+            format!("{} {}", self.temp_max, self.temp_type.to_string())
+        } else {
+            format!("{}-{} {}", self.temp_max, self.temp_min, self.temp_type.to_string())
+        }
+    }
+}
+
+impl ToString for Address {
+    fn to_string(&self) -> String {
+        if let Some(country) = self.country.as_ref() {
+            format!("{}, {}", country, self.address)
+        } else {
+            format!("{}", self.address)
+        }
+    }
+}
+
+impl ToString for Humidity {
+    fn to_string(&self) -> String {
+        format!("{}%", self.0)
+    }
+}
