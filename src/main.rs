0611643e2e6cba4f95d8acda2e7cba08292b29c9
Dmitry Sysolyatin <swonl.mail@gmail.com>
2023-03-06 22:36:00 +0200

    added help command

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,28 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
         match args[1].as_str() {
+            "help" => {
+                println!("Usage information:
+    weather <command> [<name> <param>]
+Commands:
+    help - view usage information
+    save - save default configuration
+        example:
+            weather save -provider OpenWeather -term_type C
+            weather save -address \"your address\"
+    get - view weather
+        example:
+            weather get -address -date now
+            weather get -provider OpenWeather -date \"weather date\"
+    providers - view all providers
+        example:
+            weather providers
+Configuration:
+    provider [OpenWeather] - weather provider
+    date [now, <date>] - weather date
+    address [<address>] - weather address
+    term_type [C, F, K] - temperature type (Fahrenheit, Celsius, Kelvin)");
+            },
             "save" => {
                 Config::parse_args(args).save();
             },
