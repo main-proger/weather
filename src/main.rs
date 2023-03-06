c62fbd26b9ea45b6c449f20462bbd208a0c2f202
Dmitry Sysolyatin <swonl.mail@gmail.com>
2023-03-06 22:34:54 +0200

    added command for save default config

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,24 @@
+extern crate lazy_static;
+
+use std::env;
+
+pub mod utils;
+pub mod weather;
+
+use crate::weather::config::Config;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        match args[1].as_str() {
+            "save" => {
+                Config::parse_args(args).save();
+            },
+            _ => {
+                println!("Unknown comand {}", args[1]);
+            },
+        }
+    } else {
+        println!("Arguments count error");
+    }
 }
