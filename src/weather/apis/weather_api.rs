3ab8ce195a32941d1dcb5a9b1dd537dec92cafaf
Dmitry Sysolyatin <swonl.mail@gmail.com>
2023-03-07 19:57:24 +0200

    check api type and get url for api

--- src/weather/apis/weather_api.rs
+++ src/weather/apis/weather_api.rs
@@ -1,4 +1,4 @@
-use crate::weather::config::Config;
+use crate::weather::{config::Config, info::Date};
 
 static API_KEY: &str = "9c7e143cdf584874a59173938230703";
 static API_CURRENT_URL: &str = "https://api.weatherapi.com/v1/current.xml?key={API_KEY}&q={CITY}&aqi=no";
@@ -15,4 +15,37 @@ pub struct XmlWeatherInfo {
     config: Config,
     api_type: ApiType,
     json: String,
+}
+
+impl ApiType {
+    fn from(date: &Date) -> Option<ApiType> {
+        if date.day > 10 {
+            None
+        }else if date.day == 0 {
+            Some(ApiType::Current)
+        } else {
+            Some(ApiType::Forecast)
+        }
+    }
+
+    fn api_url(&self) -> &str {
+        match self {
+            ApiType::Current => API_CURRENT_URL,
+            ApiType::Forecast => API_FORECAST_URL,
+        }
+    }
+
+    fn get_url(&self, config: &Config) -> String {
+        let url = self.api_url()
+            .replace("{API_KEY}", API_KEY)
+            .replace("{CITY}", config.address.as_ref().unwrap());
+        match self {
+            ApiType::Current => {
+                url
+            },
+            ApiType::Forecast => {
+                url.replace("{DAYS}", &config.date.as_ref().unwrap().day.to_string())
+            },
+        }
+    }
 }
